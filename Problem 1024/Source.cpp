18508e34773688ecbd54491975ce2b56a8feaa4c
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-12 19:43:54 -0800

    Add files via upload

--- Problem 1024/Source.cpp
+++ Problem 1024/Source.cpp
@@ -1,1 +1,32 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// LightOJ - Problem 1024 - EID
+// Problem Statement - http://lightoj.com/volume_showproblem.php?problem=1024
+// Main.cpp
 
+
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	priority_queue <int> pq;
+	int testCases, userInput, day;
+
+	cin >> testCases;
+	for (int i = 1; i <= testCases; i++)
+	{
+		cin >> userInput;
+		for (int i = 0; i < userInput; i++)
+		{
+			cin >> day;
+			pq.push(day);
+		}
+		cout << "Case " << i << ": " << pq.top() << endl;
+	}
+
+	cout << endl;
+	return 0;
+}
